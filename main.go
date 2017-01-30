99a85d1947ec05f94ae0eb21edcc45307370276b
Cesar Jimenez <cesar@untoldhq.com>
2017-01-29 22:28:51 -0800

    Creating modules

--- main.go
+++ main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"database/sql"
+	"github.com/Numbers88s/keltur-backend/models"
 	"fmt"
-	"log"
 	"net/http"
-	"strconv"
-	_ "github.com/lib/pq"
 )
 
-// Item is each of the item.
-type Item struct {
-	id       string
-	title    string
-	vendor   string
-	imageURL string
-	category string
-	price    float32
-}
-
-// DB is database connection.
-var DB *sql.DB
-
 func main() {
+	models.InitDB("user=keltur password=keltur dbname=keltur sslmode=disable")
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/items", itemsIndex)
-	http.HandleFunc("/items/show", itemsShow)
-	http.HandleFunc("/item/create", itemsCreate)
+	// http.HandleFunc("/items/show", itemsShow)
+	// http.HandleFunc("/item/create", itemsCreate)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -35,136 +20,106 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world from my Go program!")
 }
 
-func itemsCreate(w http.ResponseWriter, r *http.Request) {
-	var err error
-	DB, err := sql.Open("postgres", "user=keltur password=keltur dbname=keltur sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = DB.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
-
-	id := r.FormValue("id")
-	title := r.FormValue("title")
-	vendor := r.FormValue("vendor")
-	imageURL := r.FormValue("imageURL")
-	category := r.FormValue("category")
-	if id == "" || title == "" || vendor == "" || imageURL == "" || category == "" {
-    http.Error(w, http.StatusText(400), 400)
-    return
-  }
-	price, err := strconv.ParseFloat(r.FormValue("price"), 32)
-  if err != nil {
-    http.Error(w, http.StatusText(400), 400)
-    return
-  }
-
-	result, err := DB.Exec("INSERT INTO items VALUES($1, $2, $3, $4, $5, $6)", id, title, vendor, imageURL, category, price)
-  if err != nil {
-    http.Error(w, http.StatusText(500), 500)
-    return
-  }
-
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-    http.Error(w, http.StatusText(500), 500)
-    return
-  }
-
-	 fmt.Fprintf(w, "Item %s created successfully (%d row affected)\n", id, rowsAffected)
-
-}
-
-func itemsShow(w http.ResponseWriter, r *http.Request) {
-	var err error
-	DB, err := sql.Open("postgres", "user=keltur password=keltur dbname=keltur sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = DB.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
-
-	id := r.FormValue("id")
-	fmt.Printf("%+v\n", r.Form)
-	fmt.Printf("this is your %s", id)
-	if id == "" {
-		http.Error(w, http.StatusText(400), 400)
-		return
-	}
-
-	row := DB.QueryRow("SELECT * FROM items WHERE id = $1", id)
-
-	item := new(Item)
-	databaseError := row.Scan(&item.id, &item.title, &item.vendor, &item.imageURL, &item.category, &item.price)
-	if databaseError == sql.ErrNoRows {
-		http.NotFound(w, r)
-		return
-	} else if databaseError != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-
-	fmt.Printf("%s, %s, %s, %s, %s, $%.2f\n", item.id, item.title, item.vendor, item.imageURL, item.category, item.price)
-	fmt.Fprintf(w, "%s, %s, %s, %s, %s, $%.2f\n", item.id, item.title, item.vendor, item.imageURL, item.category, item.price)
-
-}
-
 func itemsIndex(w http.ResponseWriter, r *http.Request) {
-	var err error
-	DB, err := sql.Open("postgres", "user=keltur password=keltur dbname=keltur sslmode=disable")
-	// db, err := sql.Open("postgres", "postgres://keltur:keltur@localhost/keltur?sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = DB.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
 	if r.Method != "GET" {
 		http.Error(w, http.StatusText(405), 405)
-		return
 	}
 
-	rows, err := DB.Query("SELECT * FROM items")
+	items, err := models.AllItems()
 	if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-	defer rows.Close()
-
-	items := make([]*Item, 0)
-	for rows.Next() {
-		item := new(Item)
-		err := rows.Scan(&item.id, &item.title, &item.vendor, &item.imageURL, &item.category, &item.price)
-		if err != nil {
-			http.Error(w, http.StatusText(500), 500)
-			return
-		}
-		items = append(items, item)
-	}
-
-	if err = rows.Err(); err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
 	}
 
 	for _, item := range items {
-		fmt.Printf("%s, %s, %s, %s, %s, $%.2f\n", item.id, item.title, item.vendor, item.imageURL, item.category, item.price)
-		fmt.Fprintf(w, "%s, %s, %s, %s, %s, $%.2f\n", item.id, item.title, item.vendor, item.imageURL, item.category, item.price)
+		 fmt.Fprintf(w, "%s, %s, %s, %s, %s, $%.2f\n", item.Id, item.Title, item.Vendor, item.ImageURL, item.Category, item.Price)
 	}
 }
+
+// func itemsCreate(w http.ResponseWriter, r *http.Request) {
+// 	var err error
+// 	DB, err := sql.Open("postgres", "user=keltur password=keltur dbname=keltur sslmode=disable")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	if err = DB.Ping(); err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	if r.Method != "POST" {
+// 		http.Error(w, http.StatusText(405), 405)
+// 		return
+// 	}
+
+// 	id := r.FormValue("id")
+// 	title := r.FormValue("title")
+// 	vendor := r.FormValue("vendor")
+// 	imageURL := r.FormValue("imageURL")
+// 	category := r.FormValue("category")
+// 	if id == "" || title == "" || vendor == "" || imageURL == "" || category == "" {
+//     http.Error(w, http.StatusText(400), 400)
+//     return
+//   }
+// 	price, err := strconv.ParseFloat(r.FormValue("price"), 32)
+//   if err != nil {
+//     http.Error(w, http.StatusText(400), 400)
+//     return
+//   }
+
+// 	result, err := DB.Exec("INSERT INTO items VALUES($1, $2, $3, $4, $5, $6)", id, title, vendor, imageURL, category, price)
+//   if err != nil {
+//     http.Error(w, http.StatusText(500), 500)
+//     return
+//   }
+
+// 	rowsAffected, err := result.RowsAffected()
+// 	if err != nil {
+//     http.Error(w, http.StatusText(500), 500)
+//     return
+//   }
+
+// 	 fmt.Fprintf(w, "Item %s created successfully (%d row affected)\n", id, rowsAffected)
+
+// }
+
+// func itemsShow(w http.ResponseWriter, r *http.Request) {
+// 	var err error
+// 	DB, err := sql.Open("postgres", "user=keltur password=keltur dbname=keltur sslmode=disable")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	if err = DB.Ping(); err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	if r.Method != "GET" {
+// 		http.Error(w, http.StatusText(405), 405)
+// 		return
+// 	}
+
+// 	id := r.FormValue("id")
+// 	fmt.Printf("%+v\n", r.Form)
+// 	fmt.Printf("this is your %s", id)
+// 	if id == "" {
+// 		http.Error(w, http.StatusText(400), 400)
+// 		return
+// 	}
+
+// 	row := DB.QueryRow("SELECT * FROM items WHERE id = $1", id)
+
+// 	item := new(Item)
+// 	databaseError := row.Scan(&item.id, &item.title, &item.vendor, &item.imageURL, &item.category, &item.price)
+// 	if databaseError == sql.ErrNoRows {
+// 		http.NotFound(w, r)
+// 		return
+// 	} else if databaseError != nil {
+// 		http.Error(w, http.StatusText(500), 500)
+// 		return
+// 	}
+
+// 	fmt.Printf("%s, %s, %s, %s, %s, $%.2f\n", item.id, item.title, item.vendor, item.imageURL, item.category, item.price)
+// 	fmt.Fprintf(w, "%s, %s, %s, %s, %s, $%.2f\n", item.id, item.title, item.vendor, item.imageURL, item.category, item.price)
+
+// }
---
+++ models/item.go
@@ -0,0 +1,33 @@
+package models
+
+// Item is each of the item.
+type Item struct {
+	Id       string
+	Title    string
+	Vendor   string
+	ImageURL string
+	Category string
+	Price    float32
+}
+
+func AllItems() ([]*Item, error) {
+    rows, err := db.Query("SELECT * FROM items")
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    items := make([]*Item, 0)
+    for rows.Next() {
+        item := new(Item)
+        err := rows.Scan(&item.Id, &item.Title, &item.Vendor, &item.ImageURL, &item.Category, &item.Price)
+        if err != nil {
+            return nil, err
+        }
+        items = append(items, item)
+    }
+    if err = rows.Err(); err != nil {
+        return nil, err
+    }
+    return items, nil
+}
